7f45584cf204d418d23430c41983320e4f5bae3e
jlburkhead <jlburkhead@ucdavis.edu>
2014-01-13 09:21:50 -0500

    i think its working, need to diagnose and test

--- pkg/src/multilayer_perceptron.cpp
+++ pkg/src/multilayer_perceptron.cpp
@@ -1,6 +1,7 @@
 #include <RcppArmadillo.h>
 
 #include "activation.hpp"
+#include "sigmoid.hpp"
 
 using namespace Rcpp;
 
@@ -39,20 +40,25 @@ public:
     ym = as< arma::mat >(y);
     w1 = arma::randn(Xm.n_cols, n_hidden);
     w2 = arma::randn(n_hidden, ym.n_cols);
+    b1 = arma::randn(1, n_hidden);
+    b2 = arma::randn(1, ym.n_cols);
   }
   
   
   void fit(int epochs) {
     for (int e = 0; e < epochs; e++) {
       
-      arma::mat hidden_activation = sigmoid_activation(Xm, w1);
-      arma::mat output_activation = softmax_activation(hidden_activation, w2);
+      arma::mat hidden_activation = sigmoid_activation(arma::join_horiz(arma::ones(Xm.n_rows, 1), Xm), arma::join_vert(b1, w1));
+      arma::mat output_activation = softmax_activation(arma::join_horiz(arma::ones(Xm.n_rows, 1), hidden_activation), arma::join_vert(b2, w2));
       
       arma::mat delta_output = ym - output_activation;
       arma::mat delta_hidden = delta_output * w2.t() % sigmoid(hidden_activation) % (1 - sigmoid(hidden_activation));
 
       w2 = w2 - hidden_activation.t() * delta_output * learning_rate;
       w1 = w1 - Xm.t() * delta_hidden * learning_rate;
+      
+      b1 = b1 - arma::mean(delta_hidden, 0) * learning_rate;
+      b2 = b2 - arma::mean(delta_output, 0) * learning_rate;
 
     }
   }
@@ -60,12 +66,12 @@ public:
   NumericMatrix predict(NumericMatrix X) {
     arma::mat Xm = as< arma::mat >(X);
 
-    return wrap(Xm * w1 * w2);
+    return wrap(sigmoid(Xm * w1 * w2));
   }
 
 private:
   double learning_rate;
-  arma::mat Xm, ym, w1, w2;
+  arma::mat Xm, ym, w1, w2, b1, b2;
 
 };
   
